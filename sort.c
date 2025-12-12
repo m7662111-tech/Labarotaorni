f1c262b904a3c6f646869004cdd0436a50497e93
m7662111-tech <m7662111@gmail.com>
2025-12-12 12:37:31 +0700

    Changes

--- sort.c
+++ sort.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 void bubble_sort(int arr[], int n) {
     for (int i = 0; i < n-1; i++) {
         for (int j = 0; j < n-i-1; j++) {
@@ -9,5 +11,31 @@ void bubble_sort(int arr[], int n) {
         }
     }
 }
-Sortirovka pusirkon - 
-Konez
+
+cat > sort.c << 'EOF'
+#include <stdio.h>
+
+void bubble_sort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int a[] = {4, 2, 0};
+    int n = sizeof(a) / sizeof(a[0]);
+    bubble_sort(a, n);
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
